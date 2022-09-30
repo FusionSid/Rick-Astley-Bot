c64bdfe3ee8b13f3111f35b5612626bd38d4de4c
FusionSid <siddheshadsv@icloud.com>
2022-09-30 21:05:19 +1300

    removed other error

--- cogs/rickroll.py
+++ cogs/rickroll.py
@@ -57,38 +57,8 @@ class Ricklang(commands.Cog):
 
         await ctx.respond(embed=em)
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
 
-            async def better_time(cd: int):
-                time = f"{cd}s"
-                if cd > 60:
-                    minutes = cd - (cd % 60)
-                    seconds = cd - minutes
-                    minutes = int(minutes / 60)
-                    time = f"{minutes}min {seconds}s"
-                    if minutes > 60:
-                        hoursglad = minutes - (minutes % 60)
-                        hours = int(hoursglad / 60)
-                        minutes = minutes - (hours * 60)
-                        time = f"{hours}h {minutes}min {seconds}s"
-                return time
-
-            cd = round(error.retry_after)
-            if cd == 0:
-                cd = 1
-            retry_after = await better_time(cd)
-            em = discord.Embed(
-                title="Wow buddy, Slow it down\nThis command is on cooldown",
-                description=f"Try again in **{retry_after}**",
-                color=discord.Color.red(),
-            )
-            em.timestamp = datetime.datetime.utcnow()
-            error_msg = await ctx.send(embed=em)
-
-            await ctx.message.add_reaction("⚠️")
-
+    # print cooldown text
     @commands.Cog.listener()
     async def on_application_command_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
@@ -116,9 +86,8 @@ class Ricklang(commands.Cog):
                 description=f"Try again in **{retry_after}**",
                 color=discord.Color.red(),
             )
-            em.timestamp = datetime.datetime.utcnow()
-            error_msg = await ctx.send(embed=em)
 
+            em.timestamp = datetime.datetime.utcnow()
             await ctx.message.add_reaction("⚠️")
 
 
